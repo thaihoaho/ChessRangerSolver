0aa1df1ef5c673e2ce947c9c92d72317a5f5a8f7
Thái Hòa Hồ <thaihoa.hn120@gmail.com>
2025-02-19 14:50:27 +0700

    Update

--- dfs-sample.py
+++ dfs-sample.py
@@ -24,7 +24,7 @@ def filter_moves(row, col, board):
 
 def solve(pieces, start_time, depth=0):
     if len(pieces) == 1:
-        elapsed_time = time.time() - start_time  # Tính thời gian đã trôi qua
+        elapsed_time = time.time() - start_time 
         print("         " * depth + f"FOUND - Time: {elapsed_time:.6f} seconds")
         return True
     for (row, col), piece in pieces.items():
@@ -35,17 +35,11 @@ def solve(pieces, start_time, depth=0):
         for (target_x, target_y) in target:
             if (target_x, target_y) == (row, col):
                 continue
-            # Sao chép sâu (deepcopy) để tránh ảnh hưởng đến pieces gốc
             child = copy.deepcopy(pieces)
-
-            # Di chuyển quân cờ trong child
             child[(target_x, target_y)] = child.pop((row, col))
-
-            # In ra với độ sâu phù hợp để thể hiện cây
             print("         " * depth + f"({row}, {col}) -> ({target_x}, {target_y})")
-            solve(child, start_time, depth + 1)  # Gọi đệ quy với độ sâu tăng dần
+            solve(child, start_time, depth + 1)
 
-# Gọi hàm solve với dictionary pieces ban đầu
 pieces_on_board = {(2, 4): 123, (4, 2): 124, (5, 2): 125, (5.0, 4.0): 126}
 id_to_name = {
     123: 'b',
@@ -54,5 +48,5 @@ id_to_name = {
     126: 'r'
 }
 
-start_time = time.time()  # Lưu thời gian bắt đầu
+start_time = time.time() 
 solve(pieces_on_board, start_time)
